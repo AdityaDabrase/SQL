a6f1d1a4aa20391389110a4620ca344403f290d8
AdityaDabrase <46728085+AdityaDabrase@users.noreply.github.com>
2023-10-10 13:37:46 -0400

    Create expressionsVerification.sql

---
+++ Would you LIKE the Second Meal?/expressionsVerification.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+SELECT id, a, b, operation, c
+FROM expressions
+WHERE
+    (operation = '+' AND a + b = c) OR
+    (operation = '-' AND a - b = c) OR
+    (operation = '*' AND a * b = c) OR
+    (operation = '/' AND b != 0 AND a / b = c);
+END
