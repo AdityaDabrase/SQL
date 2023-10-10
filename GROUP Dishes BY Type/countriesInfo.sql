62dc312f3dacaecd5472bc3065791b0f819d7911
AdityaDabrase <46728085+AdityaDabrase@users.noreply.github.com>
2023-10-10 13:39:23 -0400

    Create countriesInfo.sql

---
+++ GROUP Dishes BY Type/countriesInfo.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SELECT count(name) as number, avg(population) as average, sum(population) as total
+	from countries;
+END
